c6b251c8478e7b6ce0e86acb6ab28c98fb7656a4
funduck007 <stepanoskin007@gmail.com>
2019-06-27 01:51:34 -0400

    add routes /square/ and /factors/

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+def factors(num):
+    return [i for i in range(1, num + 1) if num % i == 0]
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    return 'Greetings! You are on the main page!'
+
+
+@app.route('/test/<var>')
+def test_page(var):
+    print(f'You are on the test page. Variable var = {var}.')
+    return f'and capitalized, it would be {var.upper()}'
+
+
+@app.route('/square/<int:num>')
+def square_num(num):
+    return str(num ** 2)
+
+
+@app.route('/factors/<int:n>')
+def factors_route(n):
+    return render_template(
+        "factors.html",
+        num=n,
+        factors=factors(n)
+    )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
